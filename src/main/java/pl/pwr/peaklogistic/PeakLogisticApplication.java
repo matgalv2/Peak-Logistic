b5e1d90072a05dd1d11971016d5bf3dbb61041e0
Mateusz <matgalv2@gmail.com>
2022-10-05 21:11:29 +0200

    Init

---
+++ src/main/java/pl/pwr/peaklogistic/PeakLogisticApplication.java
@@ -0,0 +1,13 @@
+package pl.pwr.peaklogistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeakLogisticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeakLogisticApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/pwr/peaklogistic/PeakLogisticApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pwr.peaklogistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeakLogisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
