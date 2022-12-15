3d1fc7081550c4bebf972f880952d75951b9838a
Mateusz <matgalv2@gmail.com>
2022-12-15 20:02:02 +0100

    bug fix, dockerfile

--- src/main/java/pl/pwr/peaklogistic/config/DataSourceConfiguration.java
+++ src/main/java/pl/pwr/peaklogistic/config/DataSourceConfiguration.java
@@ -1,21 +1,21 @@
-package pl.pwr.peaklogistic.config;
-
-
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class DataSourceConfiguration {
-    @Bean
-    public DataSource getDataSource(){
-        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
-        dataSourceBuilder.url("jdbc:mysql://localhost:3306/PeakLogistics");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("pass");
-        return dataSourceBuilder.build();
-    }
-}
+//package pl.pwr.peaklogistic.config;
+//
+//
+//import org.springframework.boot.jdbc.DataSourceBuilder;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DataSourceConfiguration {
+//    @Bean
+//    public DataSource getDataSource(){
+//        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+//        dataSourceBuilder.driverClassName("com.mysql.cj.jdbc.Driver");
+//        dataSourceBuilder.url("jdbc:mysql://localhost:3306/PeakLogistics");
+//        dataSourceBuilder.username("root");
+//        dataSourceBuilder.password("pass");
+//        return dataSourceBuilder.build();
+//    }
+//}
